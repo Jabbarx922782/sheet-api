557a7c2a25519d744b732da687e2b1477e593dd2
Jabbarx922782 <islammdshafiqul8127@gmail.com>
2025-07-01 13:42:59 +0600

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,16 +1,8 @@
 const axios = require("axios");
 
-let cache = {};
-const CACHE_TIME = 5 * 60 * 1000; // 5 মিনিট কেশে রাখবে
-
 module.exports = async (req, res) => {
   const { sheetid } = req.query;
   if (!sheetid) return res.status(400).json({ error: "sheetid parameter is required" });
-
-  // Cache চেক
-  if (cache[sheetid] && (Date.now() - cache[sheetid].time < CACHE_TIME)) {
-    return res.status(200).json(cache[sheetid].data);
-  }
 
   const url = `https://docs.google.com/spreadsheets/d/${sheetid}/gviz/tq?tqx=out:json`;
 
@@ -27,17 +19,11 @@ module.exports = async (req, res) => {
     rows.forEach(row => {
       const cells = row.c;
       if (cells && cells[0] && cells[1]) {
-        const obj = {};
-        obj[cells[0].v] = cells[1].v;
-        output.push(obj);
+        const key = cells[0].v;
+        const value = cells[1].v;
+        output.push({ [key]: value });
       }
     });
-
-    // Cache সংরক্ষণ
-    cache[sheetid] = {
-      time: Date.now(),
-      data: output
-    };
 
     return res.status(200).json(output);
   } catch (err) {
